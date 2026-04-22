01b9248923645beb7293c478c431f573c0240265
Yves Casio <yveschristianpcasio@gmail.com>
2026-04-22 13:29:44 +0800

    Homepage UI

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import "../src/services/gameboard/gameboard.js";
+import "./assets/styles/buttons.css";
+import "./assets/styles/light.css";
 import "./assets/styles/modern-normalize.css";
+import "./assets/styles/resets.css";
 import "./index.css";
 
 console.log("Template is working");
